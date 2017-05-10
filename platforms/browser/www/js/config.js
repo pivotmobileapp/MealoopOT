65700d36423f67fd2d7d76360f16f4c38287706e
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2017-05-10 11:53:45 -0400

    new

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl' : "http://mealoop.com/merchantapp/api",	
+	'DialogDefaultTitle' : "Mealoop Order Taking",
+	'pushNotificationSenderid' : "946510666083",
+	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
+};
